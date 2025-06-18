5dbd10e7329aea37a8ae8fef3d1b033319f3b5e1
gnzo3e <gnzo3e@me.com>
2025-06-18 15:33:53 -0500

    Enhance basic commands display in helpcc_tui.sh by using dialog with color formatting for improved readability

--- helpcc_tui.sh
+++ helpcc_tui.sh
@@ -561,70 +561,68 @@ Stashing:
 
 # Function to display basic commands
 show_basic_commands() {
-    BASIC_TEMP_FILE=$(mktemp)
-    cat > "$BASIC_TEMP_FILE" << 'EOF'
-pwd
-  Print the current working directory
-  Example: /home/user/projects
+    dialog --title "Basic Commands" \
+           --colors \
+           --msgbox "\Z1pwd\Z0
+- Print the current working directory
+- Example: /home/user/projects
 
-ls
-  List files and directories
-  ls -l   Long format (permissions, size, date)
-  ls -la  Include hidden files (those starting with .)
-  Tip: Use ls -lh for human-readable sizes
+\Z1ls\Z0
+- List files and directories
+- \Z1ls -l\Z0: Long format (permissions, size, date)
+- \Z1ls -la\Z0: Include hidden files (those starting with .)
+- Tip: Use \Z1ls -lh\Z0 for human-readable sizes
 
-cd
-  Change directory
-  cd ~         Go to home directory
-  cd -         Go to previous directory
-  cd ../dirname  Go up one directory and into 'dirname'
-  Example: cd /tmp
+\Z1cd\Z0
+- Change directory
+- \Z1cd ~\Z0: Go to home directory
+- \Z1cd -\Z0: Go to previous directory
+- \Z1cd ../dirname\Z0: Go up one directory and into 'dirname'
+- Example: \Z1cd /tmp\Z0
 
-cp
-  Copy files or directories
-  cp file.txt /tmp/      Copy file to /tmp
-  cp -r folder/ /tmp/    Copy folder recursively
+\Z1cp\Z0
+- Copy files or directories
+- \Z1cp file.txt /tmp/\Z0: Copy file to /tmp
+- \Z1cp -r folder/ /tmp/\Z0: Copy folder recursively
 
-mv
-  Move or rename files/directories
-  mv old.txt new.txt     Rename file
-  mv file.txt /tmp/      Move file to /tmp
+\Z1mv\Z0
+- Move or rename files/directories
+- \Z1mv old.txt new.txt\Z0: Rename file
+- \Z1mv file.txt /tmp/\Z0: Move file to /tmp
 
-rm
-  Remove files or directories
-  rm file.txt            Remove file
-  rm -r folder/          Remove folder recursively
-  rm -rf folder/         Force remove folder and contents
-  Tip: Be careful with -rf!
+\Z1rm\Z0
+- Remove files or directories
+- \Z1rm file.txt\Z0: Remove file
+- \Z1rm -r folder/\Z0: Remove folder recursively
+- \Z1rm -rf folder/\Z0: Force remove folder and contents
+- Tip: Be careful with -rf!
 
-cat
-  Show contents of a file
-  Example: cat file.txt
+\Z1cat\Z0
+- Show contents of a file
+- Example: \Z1cat file.txt\Z0
 
-echo
-  Print text or variables
-  echo "Hello"           Print text
-  echo $PATH             Print variable value
+\Z1echo\Z0
+- Print text or variables
+- \Z1echo "Hello"\Z0: Print text
+- \Z1echo $PATH\Z0: Print variable value
 
-man
-  Show manual page for a command
-  Example: man ls
-  Tip: Press q to quit the manual
+\Z1man\Z0
+- Show manual page for a command
+- Example: \Z1man ls\Z0
+- Tip: Press q to quit the manual
 
-history
-  Show command history
-  Example: history
-  Tip: Use !n to repeat command number n
+\Z1history\Z0
+- Show command history
+- Example: \Z1history\Z0
+- Tip: Use \Z1!n\Z0 to repeat command number n
 
-Show PATH Directories
-  echo $PATH | tr ":" "\n"
-  Prints each directory in your PATH on a new line
-  Useful for debugging or checking your shell's search path
-  Tip: Pipe to grep to search for a directory:
-    echo $PATH | tr ":" "\n" | grep local
-EOF
-    dialog --title "Basic Commands" --textbox "$BASIC_TEMP_FILE" 25 90
-    rm -f "$BASIC_TEMP_FILE"
+\Z1Show PATH Directories\Z0
+\Z1echo $PATH | tr ":" "\\n"\Z0
+- Prints each directory in your PATH on a new line
+- Useful for debugging or checking your shell's search path
+- Tip: Pipe to grep to search for a directory:
+  \Z1echo $PATH | tr ":" "\\n" | grep local\Z0
+" 40 90
 }
 
 # Main menu
